0a7cf4dc07d910006456fbce773c80a65aea54b3
Emil Krebs <emil.krebs@outlook.de>
2024-03-17 16:21:00 +0100

    removed navbar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,25 @@
 import './globals.css';
 import { Metadata } from 'next';
 import Image from 'next/image';
-import Navbar from './components/navbar';
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
 	title: 'Emil Krebs',
 	description: 'Emil Krebs Website',
 };
 
+function Navbar() {
+	const pathname = usePathname();
+	if(pathname === '/') return null;
+	return (
+		<nav className="flex w-full justify-start items-center gap-4 p-4">
+			<Link href="/" className="border-2 rounded-lg p-4">
+                Home
+			</Link>
+		</nav>
+	);
+}
 
 export default function RootLayout({
 	children,
