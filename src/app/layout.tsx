5ad30e947bb943398c66cbd402fd5118c940dc57
Emil Krebs <emil.krebs@outlook.de>
2024-03-17 01:13:54 +0100

    build fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,18 @@ import { Montserrat } from 'next/font/google';
 import './globals.css';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
+import { Metadata } from 'next';
 
 const inter = Montserrat({ subsets: ['latin'] });
+export const metadata: Metadata = {
+	title: 'Emil Krebs',
+	description: 'Emil Krebs Website',
+};
 
 export default function RootLayout({
 	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
 	return (
 		<html lang="en">
