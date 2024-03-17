ceb0d62aaf84eb8913344e7e7dc45ce4f37bac6e
Emil Krebs <emil.krebs@outlook.de>
2024-03-17 16:19:38 +0100

    removed footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import { Metadata } from 'next';
+import Image from 'next/image';
 import Navbar from './components/navbar';
-import { Footer } from './components/footer';
 
 export const metadata: Metadata = {
 	title: 'Emil Krebs',
@@ -27,8 +27,27 @@ export default function RootLayout({
 				<Navbar />
 
 				{children}
+				
+				{/* Footer */}
+				<footer className="flex flex-col sm:flex-row items-center justify-between w-full gap-y-4 bg-black px-16 py-4">
+					<a className="uppercase text-lg hover:underline" href="/imprint">Imprint</a>
 
-				<Footer />
+					{/* GitHub */}
+					<a
+						href="https://github.com/emilkrebs/emilkrebs-website"
+						target="_blank"
+					>
+						<Image
+							className="filter-white"
+							src="/icons/github.svg"
+							alt="GitHub"
+							width={32}
+							height={32}
+						/>
+					</a>
+
+					<a className="uppercase text-lg hover:underline" href="/privacy">Privacy Policy</a>
+				</footer>
 			</body>
 		</html>
 	);
