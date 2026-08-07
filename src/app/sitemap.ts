b2aa044dc137111ce71905bca4f2a2e43f6db6cf
emil.krebs <emil.krebs@typefox.io>
2026-08-07 11:09:35 +0200

    feat: update postcss and nanoid versions; add robots and sitemap generation

---
+++ src/app/sitemap.ts
@@ -0,0 +1,30 @@
+import type { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: "https://emilkrebs.dev",
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 1,
+        },
+        {
+            url: "https://emilkrebs.dev/healthstack",
+            lastModified: new Date(),
+            changeFrequency: "monthly",
+            priority: 0.8,
+        },
+        {
+            url: "https://emilkrebs.dev/imprint",
+            lastModified: new Date(),
+            changeFrequency: "yearly",
+            priority: 0.3,
+        },
+        {
+            url: "https://emilkrebs.dev/privacy",
+            lastModified: new Date(),
+            changeFrequency: "yearly",
+            priority: 0.3,
+        },
+    ];
+}
